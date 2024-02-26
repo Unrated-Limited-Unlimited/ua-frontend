9a8a71106199b91673cc37b2139d7cc8c985c3fb
Mikal <mdr015@uib.no>
2024-02-26 14:37:11 +0100

    Started work on Whiskey-view list, fixed Themes

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -8,24 +8,40 @@
 	set_theme(theme);
     }   
 
+    function getCookie(name: string): string | undefined {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for(let i=0;i < ca.length;i++) {
+            let c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return undefined;
+    }
+
     onMount(() => {
-	const saved_theme =
-		document.documentElement.getAttribute("data-theme");
-	if (saved_theme) {
-		current_theme = saved_theme;
+	let theme =
+        getCookie("data-theme");
+        console.log("cookie theme is:"+theme)
+	if (theme) {
+		current_theme = theme;
+        set_theme(theme);
 		return;
 	}
-
-	const preference_is_dark = window.matchMedia(
+    else{
+        const preference_is_dark = window.matchMedia(
 		"(prefers-color-scheme: dracula)",
-	).matches;
+        ).matches;
 
-	const theme = preference_is_dark ? "val" : "dracula";
+        theme = preference_is_dark ? "val" : "dracula";
+    }
+
 	set_theme(theme);
     });
+
     function set_theme(theme: string) {
         const one_year = 60 * 60 * 24 * 365;
-        document.cookie = `theme=${theme}; max-age=${one_year}; path=/`;
+        document.cookie = `data-theme=${theme}; max-age=${one_year}; path=/; SameSite=None; Secure`;
         document.documentElement.setAttribute("data-theme", theme);
         current_theme = theme;
     }
@@ -35,5 +51,12 @@
     <li><a href="/">Home</a></li>
     <li><a href="/profile">Profile</a></li>
     <li><a href="/friends">Friends</a></li>
+    <li><a href="/whiskey-view">whiskey-view (testing)</a></li>
     <button on:click={toggle_theme}>random colour</button>
 </ul>
+
+<style>
+    ul {
+        margin: 0;
+    }
+</style>
