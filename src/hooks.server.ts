37d99addfe042a25a9b90c2e4802b441b4c4fa0b
Mikal <mdr015@uib.no>
2024-02-28 15:23:18 +0100

    redid the theme selector, simpler code, works better

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,12 @@
-import type { Handle } from "@sveltejs/kit";
+/** @type {import('@sveltejs/kit/hooks')} */
 
-export const handle = (async ({ event, resolve}) => {
-    let theme: string | null = null
+export const handle = async({event, resolve}) => {
 
-    const newTheme = event.url.searchParams.get("theme")
-    const cookieTheme = event.cookies.get("colortheme")
+    const theme = event.cookies.get("siteTheme");
+    const response = await resolve(event, {
+        transformPageChunk: ({html}) =>
+         html.replace('data-theme=""', `data-theme="${theme}"`),
+    });
 
-    if (newTheme) {
-        theme = newTheme
-    } else if (cookieTheme) {
-        theme = cookieTheme
-    }
-
-    if (theme) {
-        return await resolve(event, {
-            transformPageChunk: ({ html }) => html.replace('data-theme=""', `data-theme="${theme}"`)
-        })
-    }
-
-    return await resolve(event);
-}) satisfies Handle;
+    return response;
+}
