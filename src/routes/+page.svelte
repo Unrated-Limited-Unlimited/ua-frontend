04566cc884ee88c58b29a066212a0d842badeb66
Mikal <mdr015@uib.no>
2024-02-28 20:38:21 +0100

    just editing the navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <div class="main-window">
+        <h1>Welcome to Unrated</h1>
+        <p>A website for every whiskey enjoyer!</p>
+        <iframe width="1120" height="630" src="https://www.youtube.com/embed/ZeG0KbDZxwE?si=UVI1GHAlZSN-26w6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+</body>
+
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .main-window {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -6,27 +6,36 @@
 </script>
 
 <ul>
-    <li><a href="/"><img class="navbar-logo" alt="Unrated" src="/assets/VectorLogoUnratedText.svg"></a></li>
-    <li><a href="/profile">Profile</a></li>
-    <li><a href="/friends">Friends</a></li>
-    <li><a href="/whiskey-list">whiskey-list (testing)</a></li>
-    <button on:click="{() => setTheme('val')}">val Theme</button>
-    <button on:click="{() => setTheme('dracula')}">dracula Theme</button>
+    <div>
+        <li><a href="/"><img class="navbar-logo" alt="Unrated" src="/assets/VectorLogoUnratedText.svg"></a></li>
+    </div>
+    <div class="navbar-links">
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/friends">Friends</a></li>
+        <li><a href="/whiskey-list">whiskey-list (testing)</a></li>
+        <button on:click="{() => setTheme('val')}">val Theme</button>
+        <button on:click="{() => setTheme('dracula')}">dracula Theme</button>
+        </div>
 </ul>
 
 <style>
     ul {
         margin: 0;
         display:flex;
-        justify-content: flex-start;
+        justify-content: space-between;
         align-items: center;
-        gap: 3rem;
         list-style: none;
         padding: .5rem;
     }
 
     .navbar-logo {
         max-height: 3rem;
+    }
+
+    .navbar-links {
+        display: flex;
+        gap: 3rem;
+        padding: .5rem;
     }
 
     a{
