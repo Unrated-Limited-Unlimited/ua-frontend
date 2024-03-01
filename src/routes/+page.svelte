d6b7ebeedbc0e245a03a5a4f39f7d3f67a241583
mikaldr <mikal-dr@hotmail.com>
2024-03-01 12:45:36 +0100

    removed test video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     <div class="main-window">
         <h1>Welcome to Unrated</h1>
         <p>A website for every whiskey enjoyer!</p>
-        <iframe width="1120" height="630" src="https://www.youtube.com/embed/ZeG0KbDZxwE?si=UVI1GHAlZSN-26w6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 </body>
 
