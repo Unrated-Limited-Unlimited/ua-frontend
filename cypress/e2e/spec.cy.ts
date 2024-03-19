45556d9de37784a5eae15923f58fc2a686919b43
mikaldr <mikal-dr@hotmail.com>
2024-03-19 13:36:24 +0100

    New test to test test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,13 @@
 import { handlers } from '../../src/mocks/handlers';
-
+describe("Cache Test", () => {
+  it('Browserstack cookies', () =>{
+    cy.visit('http://localhost:5173')
+    cy.wait(10000)
+    cy.getCookies().should('not.be.empty')
+    cy.clearCookies()
+    cy.getCookies().should('be.empty')
+    })
+})
 describe('Login Logout Test', () => {
   it('Logs into a user and logs out', () => {
     cy.wait(20)
