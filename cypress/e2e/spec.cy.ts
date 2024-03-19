3adf9275c66a6a9e8d83a81adaad869877536876
mikaldr <mikal-dr@hotmail.com>
2024-03-19 13:41:45 +0100

    Removed test for testing test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,16 +1,8 @@
 import { handlers } from '../../src/mocks/handlers';
-describe("Cache Test", () => {
-  it('Browserstack cookies', () =>{
-    cy.visit('http://localhost:5173')
-    cy.wait(10000)
-    cy.getCookies().should('not.be.empty')
-    cy.clearCookies()
-    cy.getCookies().should('be.empty')
-    })
-})
 describe('Login Logout Test', () => {
   it('Logs into a user and logs out', () => {
-    cy.wait(20)
+    cy.wait(20000)
+    cy.clearCookies()
     cy.visit('http://localhost:5173')
     cy.contains('Profile').click()
     cy.contains('Login').click()
