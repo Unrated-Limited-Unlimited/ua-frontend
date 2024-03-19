8de7ff94dc1be9b9dae0bf85dc321f09787bbe6b
mikaldr <mikal-dr@hotmail.com>
2024-03-19 15:25:39 +0100

    Testing, removed baseurl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
