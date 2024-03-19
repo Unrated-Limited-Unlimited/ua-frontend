27a7c9cc9729596d0aaf004dd4eb4921dc0ef1a8
mikaldr <mikal-dr@hotmail.com>
2024-03-19 14:45:06 +0100

    Reverted the Login check

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,11 +1,12 @@
 import { handlers } from '../../src/mocks/handlers';
+
 describe('Login Logout Test', () => {
   it('Logs into a user and logs out', () => {
     cy.clearCookies()
     cy.clearAllLocalStorage()
     cy.visit('http://localhost:5173')
     cy.contains('Profile').click()
-    cy.get('a[name="login"]').click()
+    cy.contains('Login').click()
     cy.get('input[name="username"]').type('fake@email.com')
     cy.get('input[name="password"]').type('password123')
     cy.contains('button[name="login"]').click()
