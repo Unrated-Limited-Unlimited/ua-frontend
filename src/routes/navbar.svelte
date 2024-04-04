88a4d6693259f60846482511fa4ffc3fb9d43c6b
Martin <591337@stud.hvl.no>
2024-04-04 19:25:29 +0200

    chosing logo with flag

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,11 +1,33 @@
+<script lang="ts">
+    import flagsmith from 'flagsmith';
+    
+    export let fancyLogo = false;
+
+    flagsmith.init({
+        environmentID: "3DGuwpd47RjMmj9ajBh8uU",
+        cacheFlags: true,
+        onChange() {
+            fancyLogo = flagsmith.hasFeature("logo-change")
+        },
+    })
+</script>
+
 <ul>
     <div>
-        <li><a href="/"><img class="navbar-logo" alt="Unrated" src="/assets/VectorLogoUnratedText.svg"></a></li>
+        <li>
+            <a href="/">
+            {#if fancyLogo}
+                <img class="navbar-logo" alt="Unrated" src="/assets/Logo_4k.png">
+            {:else}
+                <img class="navbar-logo" alt="Unrated" src="/assets/VectorLogoUnratedText.svg">
+            {/if}
+            </a>
+        </li>
     </div>
     <div class="navbar-links">
         <li><a href="/profile">Profile</a></li>
         <li><a href="/whiskey">Whiskey</a></li>
-        </div>
+    </div>
 </ul>
 
 <style>
