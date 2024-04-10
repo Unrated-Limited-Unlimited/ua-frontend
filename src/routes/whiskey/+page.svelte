b32f36a7ac422619ab6faaff85a607c655c55f5f
mikaldr <mikal-dr@hotmail.com>
2024-04-10 15:36:28 +0200

    Testing new layout for whiskey list

--- src/routes/whiskey/+page.svelte
+++ src/routes/whiskey/+page.svelte
@@ -10,40 +10,47 @@
     {#if data.whiskey_list}
     <div>
         <h1>{ capitalize($featureFlagStore?.wiskeySpelling) }</h1>
-        {#each data.whiskey_list as whiskey}
-        <a id="whiskey-link" href="/whiskey/{whiskey.id}">
-            <div class="whiskey-view-container">
-                <div class="whiskey-view-image-container">
-                    <img class="whiskey-view-image" alt={whiskey.title} src={(whiskey.img)}>
-                </div>
-                <div class="whiskey-view-desc">
-                    <h3>
-                        {whiskey.title}
-                    </h3>
-                    <div class="stars">
-                        {#each Array(Math.round(whiskey.avgScore*5)) as _, index}
-                        <svg class="rating-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <path 
-                                    fill="currentColor" 
+        <div class="grid-container">
+            {#each data.whiskey_list as whiskey}
+            <a id="whiskey-link" href="/whiskey/{whiskey.id}">
+                <div class="whiskey-view-container">
+                    <div class="whiskey-view-image-container">
+                        <img class="whiskey-view-image" alt={whiskey.title} src={(whiskey.img)}>
+                    </div>
+                    <div class="whiskey-view-desc">
+                        <h3>
+                            {whiskey.title}
+                        </h3>
+                        <div class="stars">
+                            {#each Array(Math.round(whiskey.avgScore*5)) as _, index}
+                            <svg class="rating-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path 
+                                        fill="currentColor" 
+                                        d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"/>
+                                </svg>
+                            {/each}
+                            {#each Array(5-Math.round(whiskey.avgScore*5)) as _, index}
+                            <svg class="unfill-rating-star rating-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path
                                     d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"/>
                             </svg>
-                        {/each}
-                        {#each Array(5-Math.round(whiskey.avgScore*5)) as _, index}
-                        <svg class="unfill-rating-star rating-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                            <path
-                                d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"/>
-                        </svg>
-                        {/each}
+                            {/each}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </a>
-        {/each}
+            </a>
+            {/each}
+        </div>
     </div>
     {/if}
 </body>
 
 <style>
+    .grid-container {
+        display: grid;
+        grid-template-columns: 25% 25% 25% 25%;
+        gap: .5rem;
+    }
     body {
         margin: 2rem;
     }
@@ -55,16 +62,16 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        width:10rem;
+        width:auto;
         height: auto;
     }
     .whiskey-view-container {
+        margin: 0;
         background-color: var(--navbar);
         display: flex;
         justify-content:flex-start;
         gap: 1rem;
         flex-direction:row;
-        margin: .5rem;
         border-radius: 2rem;
         padding: .5rem;
         color: var(--contrast-text)
@@ -81,6 +88,7 @@
         max-width: 7rem;
         max-height: 7rem;
         min-width: 4rem;
+        width: auto;
         height: auto;
         padding: .5rem;
         display: flex;
@@ -90,7 +98,8 @@
     .whiskey-view-image {
         max-height: 7rem;
         max-width: 7rem;
-        min-width: 2rem;
+        min-width: 3rem;
+        width: auto;
         height: auto;
     }
 
