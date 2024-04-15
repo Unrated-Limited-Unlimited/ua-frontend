6c0454642fbdc0debd762ab47e255cc1c340ec5d
MikalDr <mikal-dr@hotmail.com>
2024-04-15 13:22:00 +0200

    started work on responsive whiskey page

--- src/routes/whiskey/[id]/+page.svelte
+++ src/routes/whiskey/[id]/+page.svelte
@@ -1,65 +1,141 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import { onMount, onDestroy } from 'svelte';
+
   export let data: PageData;
   export let id = data.id;
   let roundScore = Math.round(data.whiskey.avgScore*5);
-
+  let shortenedSummary: boolean = true;
   let categories = data.whiskey.categories;
 
   let reviews = data.whiskey.ratings;
+
+  let isMobile = false;
+
+  function checkWindowSize() {
+      // Example breakpoint at 640px
+      isMobile = window.innerWidth < 640;
+  }
+
+  onMount(() => {
+      checkWindowSize(); // Check size on mount
+      window.addEventListener('resize', checkWindowSize);
+  });
+
+  onDestroy(() => {
+      window.removeEventListener('resize', checkWindowSize);
+  });
+
+  function summarySwap() {
+    shortenedSummary = !shortenedSummary;
+  }
+
+  function truncateString(str: string, maxLength: number, useEllipsis: boolean = true): string {
+    if (str.length > maxLength) {
+        return str.substring(0, maxLength) + (useEllipsis ? '...' : '');
+    }
+    return str;
+    }
 </script>
 
 <title>{data.whiskey.title} - Unrated</title>
 
 <div class="flex-column whiskey-site">
   <div class="main-box">
-    <div class="whiskey-image">
-      <img
-        class="whiskey-image"
-        alt={data.whiskey.name}
-        src={data.whiskey.img}
-      />
-    </div>
-    <div>
-      <!--<p>{data.whiskey.producer}</p>-->
-      <h2>{data.whiskey.title}</h2>
-      <div class="centered flex-inline">
-        <h1>{roundScore}</h1>
-        <div class="stars">
-          {#each Array(roundScore) as _}
-            <svg
-              class="rating-star"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
-              />
-            </svg>
-          {/each}
-          {#each Array(5 - roundScore) as _}
-            <svg
-              class="unfill-rating-star2 rating-star"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path
-                d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
-              />
-            </svg>
-          {/each}
-        </div>
-        <a href="/whiskey/{id}/rate">Rate this whiskey!</a>
+    {#if isMobile}
+    <div class="small-window">
+      <div class="whiskey-image">
+        <img
+          class="whiskey-image"
+          alt={data.whiskey.name}
+          src={data.whiskey.img}
+        />
       </div>
-      <p>{data.whiskey.summary}</p>
-      <!--
-                currently no favourite system implemented
-                <button>Favourite</button>
-                -->
+      <div class="whiskey-details">
+      
+        <!--<p>{data.whiskey.producer}</p>-->
+        <h2>{data.whiskey.title}</h2>
+        <div class="centered flex-inline score-box">
+          <h1>{roundScore}</h1>
+          <div class="stars">
+            {#each Array(roundScore) as _}
+              <svg
+                class="rating-star"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
+                />
+              </svg>
+            {/each}
+            {#each Array(5 - roundScore) as _}
+              <svg
+                class="unfill-rating-star2 rating-star"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
+                />
+              </svg>
+            {/each}
+          </div>
+          <a href="/whiskey/{id}/rate">Rate this whiskey!</a>
+        </div>
+      </div>
     </div>
+      {#if shortenedSummary}
+        <p>{truncateString(data.whiskey.summary, 100, false)}  <a href="" on:click={summarySwap}> [more]</a></p>
+      {:else}
+        <p>{data.whiskey.summary}  <a href="" on:click={summarySwap}> [less]</a></p>
+      {/if}
+    {:else}
+      <div class="whiskey-image">
+        <img
+          class="whiskey-image"
+          alt={data.whiskey.name}
+          src={data.whiskey.img}
+        />
+      </div>
+      <div>
+        <!--<p>{data.whiskey.producer}</p>-->
+        <h2>{data.whiskey.title}</h2>
+        <div class="centered flex-inline">
+          <h1>{roundScore}</h1>
+          <div class="stars">
+            {#each Array(roundScore) as _}
+              <svg
+                class="rating-star"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
+                />
+              </svg>
+            {/each}
+            {#each Array(5 - roundScore) as _}
+              <svg
+                class="unfill-rating-star2 rating-star"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M21.5,9.757l-5.278,4.354L17.871,21.5,12,17.278,6.129,21.5l1.649-7.389L2.5,9.757l6.333-.924L12,2.5l3.167,6.333Z"
+                />
+              </svg>
+            {/each}
+          </div>
+          <a href="/whiskey/{id}/rate">Rate this whiskey!</a>
+        </div>
+        <p>{data.whiskey.summary}</p>
+    </div>
+    {/if}
   </div>
-  <div class="centered flex-column contrast-box">
+  <div class="centered flex-column contrast-box taste-profile">
     <h3>Taste profile</h3>
     <div class="sliders">
       {#each categories as category}
@@ -128,18 +204,29 @@
     fill: var(--navbar);
   }
   .taste-profile {
-    color: var(--contrast-text);
-    padding-bottom: 1.5rem;
+    padding-bottom: 2rem;
   }
   .main-box {
     background-color: var(--bg-color);
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
     width: 100%;
     gap: 2rem;
     padding: 2rem;
     margin: 0;
+    text-wrap: wrap;
+
+    p {
+      width: 30rem;
+      max-width: 30rem;
+
+      a {
+        text-decoration: none;
+        opacity: 0.7;
+      }
+    }
   }
   .stars {
     display: flex;
@@ -160,6 +247,8 @@
   .whiskey-image {
     max-height: 35rem;
     max-width: auto;
+    border-radius: 2rem;
+    border: 2rem solid var(--white);
   }
 
   .slider {
@@ -202,5 +291,49 @@
   }
   .sliders {
     width: 30%;
+  }
+
+  @media only screen and (max-width: 639px) {
+    .main-box {
+      display: flex;
+      flex-direction: column;
+      margin: 0;
+      gap: 0rem;
+      padding: 0;
+      padding-top: 2vh;
+      p{
+        width:100%;
+        max-width: 20rem;
+      }
+
+      .small-window{
+        display: flex;
+      }
+
+      .score-box {
+        display: flex;
+        flex-direction: column;
+        gap: 0.2rem;
+        justify-content: center;
+        align-items: center;
+        margin: 3vw;
+      }
+
+      .whiskey-details {
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+    .whiskey-image {
+      max-height: 20rem;
+    }
+  }
+  @media only screen and (min-width: 640px) {
+
+  }
+  @media only screen and (min-width: 1200px) {
+
   }
 </style>
